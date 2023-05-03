1494b1f28f8208edaeaeb7a6c8d98dda88971037
Emma-Marie <emma-marie@outlook.dk>
2023-05-03 16:25:18 +0200

    ass5 begun

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#create virtual environment
+#python3 -m venv sermons_env
+#source ./sermons_env/bin/activate
+
+# requirements
+pip install --upgrade pip
+pip install --upgrade nbformat
+python3 -m pip install -r requirements.txt
+
+#deactivate the venv
+#deactivate
